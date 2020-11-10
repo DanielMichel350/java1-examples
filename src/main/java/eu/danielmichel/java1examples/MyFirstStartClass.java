9871a27f220d596b0b9f5b797fe104d4b3b5c2e2
DanielMichel350 <daniel.michel@posteo.de>
2020-11-10 11:21:48 +0100

    Grundeinstellungen

---
+++ src/main/java/eu/danielmichel/java1examples/MyFirstStartClass.java
@@ -0,0 +1,10 @@
+package eu.danielmichel.java1examples;
+
+class MyFirstStartClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
+
---
+++ src/test/java/eu/danielmichel/java1examples/MyFirstStartClassTest.java
@@ -0,0 +1,12 @@
+package eu.danielmichel.java1examples;
+
+class MyFirstStartClassTest {
+
+    void check_if_something_does_something() {
+        // Given
+
+        // When
+
+        // Then
+    }
+}
