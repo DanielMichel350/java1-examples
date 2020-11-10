cdcfae80cf8e189b9be26f7da725198f75725fda
DanielMichel350 <daniel.michel@posteo.de>
2020-11-10 11:46:58 +0100

    FitzQuark

--- src/main/java/eu/danielmichel/java1examples/MyFirstStartClass.java
+++ src/main/java/eu/danielmichel/java1examples/MyFirstStartClass.java
@@ -3,7 +3,24 @@ package eu.danielmichel.java1examples;
 class MyFirstStartClass {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        String output = "";
+
+        for(int i = 1; i<=100; i++){
+            if (i%5 == 0){
+                output = output.concat("Fitz");
+            }
+            if(i%7 == 0){
+                output = output.concat("Quark");
+            }
+            if(output.equals("")){
+                System.out.println(i);
+            }
+            else{
+                System.out.println(output);
+                output = "";
+            }
+        }
+
     }
 
 }
